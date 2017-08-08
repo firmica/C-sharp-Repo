db53d239cb3c9ce61cdbb8e49008eaf9f9a686ff
Nikola Zivaljevic <dzonikg90@gmail.com>
2017-08-08 13:04:11 +0200

    Code refactored, deleted some classes

--- Backend/Components/Group/GroupsCrud.cs
+++ Backend/Components/Group/GroupsCrud.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Backend.GeneratedClasses;
 using Backend.GeneratedClasses.Requests;
-using Backend.GeneratedClasses.Responses;
 
 namespace Backend.Components.Entry
 {
